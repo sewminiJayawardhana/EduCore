33fe63a0690b2bafb2045eb3095c6ff31e533895
Sewmini <sewuminijayawardana23@gmail.com>
2025-08-02 22:36:57 +0530

    Basic structure

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <img src={logo} alt="" />
+        <ul>
+            <li>Home</li>
+            <li>Program</li>
+            <li>Campus</li>
+            <li>Reviews</li>
+            <li>Contact us</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
