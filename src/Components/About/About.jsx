b6196bdf72026b6629b988a050f32fe3f4ca3e41
Sewmini <sewuminijayawardana23@gmail.com>
2025-08-04 00:01:47 +0530

    Refactor Title component to accept subtitle and title props.

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <div className="about-left">
+            <img src= {about_img} alt="" className='about-img' />
+            <img src= {play_icon} alt="" className='play-icon'/>
+        </div>
+        <div className="about-right">
+            <h3>About University</h3>
+            <h2>Nurturing Tomorrow's Leaders Today</h2>
+        </div>
+      
+    </div>
+  )
+}
+
+export default About
