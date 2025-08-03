b21e5eef5ffebe54b5ca494bc71790fbef98c85f
Sewmini <sewuminijayawardana23@gmail.com>
2025-08-03 23:18:00 +0530

    `Added Title component and modified Programs component styles`

---
+++ src/Components/Title/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Title.css'
+
+const Title = () => {
+  return (
+    <div className='title'>
+      <p> Our Program</p>
+      <h2>What we Offer</h2>
+    </div>
+  )
+}
+
+export default Title
