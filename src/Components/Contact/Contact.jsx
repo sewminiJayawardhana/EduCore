b20eba2af2d5d27b728d0987774b0e952b8d1120
Sewmini <sewuminijayawardana23@gmail.com>
2025-08-06 01:54:09 +0530

    Added content to Contact component and updated CSS styles.

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import'./Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
 
 const Contact = () => {
   return (
     <div className='contact'>
-        <div className="contact-col"></div>
+        <div className="contact-col">
+            <h3>Send us a message <img src={msg_icon} alt="" /></h3>
+            <p>
+               We’d love to hear from you! Whether you have a question, suggestion, or need support, feel free to reach out using the form below. We're here to help and always ready to connect with our university community. 
+            </p>
+            <ul>
+                <li>educore@gmail.com</li>
+                <li>+94 772 056 235</li>
+                <li>Kandy Road, Kaduwela,Sri Lanka</li>
+            </ul>
+        </div>
       
     </div>
   )
