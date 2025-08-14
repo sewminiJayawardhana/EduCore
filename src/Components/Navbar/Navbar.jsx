4dcf258bf8c7711d671c2eb543eeffeacc4ff218
sewmini <139870212+sewminiJayawardhana@users.noreply.github.com>
2025-08-14 22:46:10 +0530

    Updated Navbar.css and Navbar.jsx with minor changes to styles and link offsets.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -18,11 +18,11 @@ const Navbar = () => {
         <img src={logo} alt="" className='logo' />
         <ul>
             <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
-            <li><Link to='programs' smooth={true} offset={0} duration={500}>Program</Link></li>
-            <li><Link to='about' smooth={true} offset={0} duration={500}>About</Link></li>
-            <li><Link to='campus' smooth={true} offset={0} duration={500}>Campus</Link></li>
-            <li><Link to='testimonials' smooth={true} offset={0} duration={500}>Testimonials</Link></li>
-            <li><Link to='contact' smooth={true} offset={0} duration={500} className='btn'>Contact us</Link></li>
+            <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
+            <li><Link to='about' smooth={true} offset={-150} duration={500}>About</Link></li>
+            <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
+            <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+            <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
         </ul>
     </nav>
   )
