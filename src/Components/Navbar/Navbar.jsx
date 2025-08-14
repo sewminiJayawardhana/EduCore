11779a4e35ba6c206957112b928a031408063a4c
sewmini <139870212+sewminiJayawardhana@users.noreply.github.com>
2025-08-14 23:16:45 +0530

    Added mobile menu toggle functionality and CSS styles for responsive design

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -14,10 +14,17 @@ const Navbar = () => {
     })
   },[])
 
+  //Initial value of the mobile menu
+
+  const[mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = ()=>{
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  }
+
   return (
     <nav className={`container ${sticky? 'dark-nav': ''}`} >
         <img src={logo} alt="" className='logo' />
-        <ul>
+        <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
             <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
             <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
             <li><Link to='about' smooth={true} offset={-150} duration={500}>About</Link></li>
@@ -25,7 +32,7 @@ const Navbar = () => {
             <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
             <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
         </ul>
-        <img src={menu_icon} alt=""className='menu-icon' />
+        <img src={menu_icon} alt=""className='menu-icon' onClick={toggleMenu} />
     </nav>
   )
 }
